3c90cde4871f631103c76e4b7fb776cfeb1d2dea
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-04 10:49:44 +0200

    FEAT: iteration6

---
+++ Iteration6.js
@@ -0,0 +1,31 @@
+/* Crea una función que reciba por parámetro un array y compruebe si
+existen elementos duplicados, en caso que existan los elimina para
+retornar un array sin los elementos duplicados. Puedes usar este
+array para probar tu función: */
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+
+  function removeDuplicates(arr) {
+    noDup = []
+    for (let i = 0; i < arr.length; i++) {
+        if (noDup.indexOf(arr[i]) == -1) {
+            noDup.push(arr[i])
+        }
+    }
+
+    return noDup
+  }
+
+console.log(removeDuplicates(duplicates));
